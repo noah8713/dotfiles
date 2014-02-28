8564779625292265b633e2e360de4835588bbef4
Qiu Yu <unicell@gmail.com>
2014-02-28 16:51:23 +0800

    Change firsttime_reminder.sh and save for future use

--- conf/firsttime_reminder.sh
+++ conf/firsttime_reminder.sh
@@ -2,10 +2,6 @@
 shopt -s expand_aliases
 source ~/.bashrc
 
-# I'm forgetful. Just look at this repo's commits to see how many times I
-# forgot to setup Git and GitHub.
-
 cat <<EOF
-If this is a remote server, run:
-ssh-copy-id $USER@$(wanip) && ssh $USER@$(wanip)
+Don't Panic!
 EOF
